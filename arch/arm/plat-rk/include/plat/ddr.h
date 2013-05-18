864bf15d2dcfdef37b0281c611c15b66fe5b21ca
Rockchip <service@rock-chips.com>
2013-05-18 09:11:45 +0800

    RK2928:resolve compiled ddr.c fail for rk2928
    Conflicts:
    	arch/arm/plat-rk/include/plat/ddr.h

--- arch/arm/plat-rk/include/plat/ddr.h
+++ arch/arm/plat-rk/include/plat/ddr.h
@@ -153,9 +153,6 @@ struct ddr_freq_t {
 void __sramfunc ddr_suspend(void);
 void __sramfunc ddr_resume(void);
 //void __sramlocalfunc delayus(uint32_t us);
-#if defined(CONFIG_ARCH_RK2928) || defined(CONFIG_ARCH_RK3026)
-uint32_t __sramfunc ddr_change_freq(uint32_t nMHz);
-#else
 uint32_t ddr_change_freq(uint32_t nMHz);
 uint32_t __sramfunc ddr_change_freq_sram(uint32_t nMHz , struct ddr_freq_t ddr_freq_t);
 uint32_t ddr_get_cap(void);
