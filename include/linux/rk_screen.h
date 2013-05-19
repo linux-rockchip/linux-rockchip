ff6e982730e38d4509db9c513e827b3cd48160a7
Rockchip <service@rock-chips.com>
2013-05-19 16:20:54 +0800

    rk screen:add rk616 support

--- include/linux/rk_screen.h
+++ include/linux/rk_screen.h
@@ -44,12 +44,13 @@ Y	TX11   	G7		G1		GND		GND
 */
 
 typedef enum _SCREEN_TYPE {
-    SCREEN_NULL = 0,
-    SCREEN_RGB,
-    SCREEN_LVDS,
+	SCREEN_NULL = 0,
+	SCREEN_RGB,
+	SCREEN_LVDS,
 	SCREEN_MCU,
-    SCREEN_TVOUT,
-    SCREEN_HDMI,
+	SCREEN_TVOUT,
+	SCREEN_HDMI,
+	SCREEN_MIPI,
 } SCREEN_TYPE;
 
 typedef enum _REFRESH_STAGE {
@@ -122,6 +123,7 @@ typedef struct rk29fb_screen {
 	u32 mode;
 	/* Timing */
 	u32 pixclock;
+	u32 fps;
 	u16 left_margin;
 	u16 right_margin;
 	u16 hsync_len;
@@ -130,6 +132,7 @@ typedef struct rk29fb_screen {
 	u16 vsync_len;
 	u8  ft;	//the time need to display one frame,in ms
 	int *dsp_lut; //display lut 
+	struct rk29fb_screen *ext_screen;
 #if defined(CONFIG_HDMI_DUAL_DISP) || defined(CONFIG_ONE_LCDC_DUAL_OUTPUT_INF)
     /* Scaler mode Timing */
 	u32 s_pixclock;
@@ -144,6 +147,15 @@ typedef struct rk29fb_screen {
 	bool s_den_inv;
 	bool s_hv_sync_inv;
 	bool s_clk_inv;
+#endif
+
+#if defined(CONFIG_MFD_RK616)
+	u32 pll_cfg_val;  //bellow are for jettaB
+	u32 frac;
+	u16 scl_vst;
+	u16 scl_hst;
+	u16 vif_vst;
+	u16 vif_hst;
 #endif
 	u8 hdmi_resolution;
 	    /* mcu need */
