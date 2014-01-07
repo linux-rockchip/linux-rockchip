99470dbe5fdc66c9dc09e46a68f6e4fb52c722d2
Rockchip <service@rock-chips.com>
2014-01-07 14:28:24 +0800

    WIFI&BT:Update wifi&bt drivers         Support more modules,OOB for wifi modules         BT4.0,it depends on the module support BT4.0

--- arch/arm/plat-rk/rk-sdmmc-wifi.c
+++ arch/arm/plat-rk/rk-sdmmc-wifi.c
@@ -341,12 +341,17 @@ static int __init rk29sdk_wifi_bt_gpio_control_init(void)
     rk29sdk_init_wifi_mem();    
     rk29_mux_api_set(rk_platform_wifi_gpio.power_n.iomux.name, rk_platform_wifi_gpio.power_n.iomux.fgpio);
 
+#ifdef CONFIG_MACH_RK_FAC
+	if(wifi_pwr!=-1)
+		port_output_init(wifi_pwr, 1, "wifi_pwr"); 
+#else
     if (rk_platform_wifi_gpio.power_n.io != INVALID_GPIO) {
         if (gpio_request(rk_platform_wifi_gpio.power_n.io, "wifi_power")) {
                pr_info("%s: request wifi power gpio failed\n", __func__);
                return -1;
         }
     }
+#endif
 
 #ifdef RK30SDK_WIFI_GPIO_RESET_N
     if (rk_platform_wifi_gpio.reset_n.io != INVALID_GPIO) {
@@ -358,8 +363,13 @@ static int __init rk29sdk_wifi_bt_gpio_control_init(void)
     }
 #endif    
 
-    if (rk_platform_wifi_gpio.power_n.io != INVALID_GPIO)
+#ifdef CONFIG_MACH_RK_FAC
+	if(wifi_pwr!=-1)
+		port_output_off(wifi_pwr);
+#else
+	if (rk_platform_wifi_gpio.power_n.io != INVALID_GPIO)
         gpio_direction_output(rk_platform_wifi_gpio.power_n.io, !(rk_platform_wifi_gpio.power_n.enable) );
+#endif
 
 #ifdef RK30SDK_WIFI_GPIO_RESET_N 
     if (rk_platform_wifi_gpio.reset_n.io != INVALID_GPIO)
@@ -396,22 +406,32 @@ static int __init rk29sdk_wifi_bt_gpio_control_init(void)
 }
 
 #if (defined(CONFIG_RTL8192CU) || defined(CONFIG_RTL8188EU) || defined(CONFIG_RTL8723AU)) \
-	&& defined(CONFIG_ARCH_RK2928)
+	&& (defined(CONFIG_ARCH_RK2928) || defined(CONFIG_MACH_RK3026_86V))
 static int usbwifi_power_status = 1;
 int rk29sdk_wifi_power(int on)
 {
         pr_info("%s: %d\n", __func__, on);
          if (on){
+            #if defined(CONFIG_USB_WIFI_POWER_CONTROLED_BY_GPIO)
+                gpio_set_value(rk_platform_wifi_gpio.power_n.io, rk_platform_wifi_gpio.power_n.enable);
+                mdelay(100);
+            #else
                 if(usbwifi_power_status == 1) {
                     rkusb_wifi_power(0);
                     mdelay(50);
                 }
                 rkusb_wifi_power(1);
+            #endif
                 usbwifi_power_status = 1;
                  pr_info("wifi turn on power\n");  	
         }else{
+            #if defined(CONFIG_USB_WIFI_POWER_CONTROLED_BY_GPIO)
+                gpio_set_value(rk_platform_wifi_gpio.power_n.io, !(rk_platform_wifi_gpio.power_n.enable));
+                mdelay(100);
+            #else
                 rkusb_wifi_power(0);
                 usbwifi_power_status = 0;    	
+            #endif
                  pr_info("wifi shut off power\n");
         }
         return 0;
@@ -421,7 +441,12 @@ int rk29sdk_wifi_power(int on)
 {
         pr_info("%s: %d\n", __func__, on);
         if (on){
+			#ifdef CONFIG_MACH_RK_FAC
+				if(wifi_pwr!=-1)
+					port_output_on(wifi_pwr);
+			#else
                 gpio_set_value(rk_platform_wifi_gpio.power_n.io, rk_platform_wifi_gpio.power_n.enable);
+			#endif
                 mdelay(50);
 
                 #if defined(CONFIG_SDMMC1_RK29) && !defined(CONFIG_SDMMC_RK29_OLD)	
@@ -436,8 +461,12 @@ int rk29sdk_wifi_power(int on)
                 pr_info("wifi turn on power\n");
         }else{
 //                if (!rk29sdk_bt_power_state){
-                        gpio_set_value(rk_platform_wifi_gpio.power_n.io, !(rk_platform_wifi_gpio.power_n.enable));
-
+				#ifdef CONFIG_MACH_RK_FAC
+						if(wifi_pwr!=-1)
+							port_output_off(wifi_pwr);
+				#else
+						gpio_set_value(rk_platform_wifi_gpio.power_n.io, !(rk_platform_wifi_gpio.power_n.enable));
+				#endif
                         #if defined(CONFIG_SDMMC1_RK29) && !defined(CONFIG_SDMMC_RK29_OLD)	
                         rk29_sdmmc_gpio_open(1, 0); //added by xbw at 2011-10-13
                         #endif
@@ -482,12 +511,38 @@ int rk29sdk_wifi_set_carddetect(int val)
 }
 EXPORT_SYMBOL(rk29sdk_wifi_set_carddetect);
 
-#define WIFI_HOST_WAKE RK30_PIN3_PD2
+#include <linux/etherdevice.h>
+u8 wifi_custom_mac_addr[6] = {0,0,0,0,0,0};
+extern char GetSNSectorInfo(char * pbuf);
+static int rk29sdk_wifi_mac_addr(unsigned char *buf)
+{
+    printk("rk29sdk_wifi_mac_addr.\n");
+    
+	// from vflash
+    if(is_zero_ether_addr(wifi_custom_mac_addr)) {
+    	int i;
+    	char *tempBuf = kmalloc(512, GFP_KERNEL);
+    	if(tempBuf) {
+    		GetSNSectorInfo(tempBuf);
+			for (i = 506; i <= 511; i++)
+				wifi_custom_mac_addr[i-506] = tempBuf[i];
+			kfree(tempBuf);
+    	}
+	}
+	
+	memcpy(buf, wifi_custom_mac_addr, 6);
+	return 0;
+}
+EXPORT_SYMBOL(rk29sdk_wifi_mac_addr);
+
+//#define WIFI_HOST_WAKE RK30_PIN3_PD2
 
 static struct resource resources[] = {
 	{
-		.start = WIFI_HOST_WAKE,
-		.flags = IORESOURCE_IRQ,
+#ifdef RK30SDK_WIFI_GPIO_WIFI_INT_B
+		.start = RK30SDK_WIFI_GPIO_WIFI_INT_B,
+#endif
+		.flags = IORESOURCE_IRQ | IORESOURCE_IRQ_HIGHLEVEL | IORESOURCE_IRQ_SHAREABLE,
 		.name = "bcmdhd_wlan_irq",
 	},
 };
