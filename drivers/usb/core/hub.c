95bde45c630865d83812031ea8aab4b5add14382
Rockchip <service@rock-chips.com>
2014-09-16 18:19:39 +0800

    usb : usb patch from server

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -1689,14 +1689,16 @@ static int hub_probe(struct usb_interface *intf, const struct usb_device_id *id)
 	 * where the controller driver doesn't have bus_suspend and
 	 * bus_resume methods.
 	 */
-	if (hdev->parent) {		/* normal device */
-		usb_enable_autosuspend(hdev);
-	} else {			/* root hub */
-		const struct hc_driver *drv = bus_to_hcd(hdev->bus)->driver;
-
-		if (drv->bus_suspend && drv->bus_resume)
-			usb_enable_autosuspend(hdev);
-	}
+	
+	// modified to close the auto suspend of hub. 
+	//if (hdev->parent) {		/* normal device */
+	//	usb_enable_autosuspend(hdev);
+	//} else {			/* root hub */
+	//	const struct hc_driver *drv = bus_to_hcd(hdev->bus)->driver;
+	//
+	//	if (drv->bus_suspend && drv->bus_resume)
+	//		usb_enable_autosuspend(hdev);
+	//}
 
 	if (hdev->level == MAX_TOPO_LEVEL) {
 		dev_err(&intf->dev,
